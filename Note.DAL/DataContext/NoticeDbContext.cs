0184fe7a8353ee0256ce7c968489093ac3828131
soo.toance <soo.toance@gmail.com>
2020-03-22 12:48:01 +0900

    EntityFramework 설치 및 연동

---
+++ Note.DAL/DataContext/NoticeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Note.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Note.DAL.DataContext
+{
+    class NoticeDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Notice> Notices { get; set; }
+
+        public NoticeDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("LocalDb"));
+        }
+    }
+}
---
+++ Note.IDAL/INoticeDal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Note.DAL;
+using Note.Model;
+
+namespace Note.DAL
+{
+    public interface INoticeDal
+    {
+        /// <summary>
+        /// 공지사항 리스트 출력
+        /// </summary>
+        /// <returns></returns>
+        List<Notice> GetNoticeList();
+
+        /// <summary>
+        /// 공지사항 상세 출력
+        /// </summary>
+        /// <param name="noticeNo"></param>
+        /// <returns></returns>
+        Notice GetNotice(int noticeNo);
+
+        /// <summary>
+        /// 공지사항 등록 
+        /// </summary>
+        /// <param name="notice"></param>
+        /// <returns></returns>
+        bool PostNotice(Notice notice);
+
+        /// <summary>
+        /// 공지사항 수정 
+        /// </summary>
+        /// <param name="notice"></param>
+        /// <returns></returns>
+        bool UpdateNotice(Notice notice);
+
+        /// <summary>
+        /// 공지사항 삭제 
+        /// </summary>
+        /// <param name="noteiceNo"></param>
+        /// <returns></returns>
+        bool DeleteNotice(int noticeNo);
+
+    }
+}
